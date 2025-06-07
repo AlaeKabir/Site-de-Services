4610ff290d114edb0aa06c9ddaf8cd2c48b4f32d
AlaeKabir <kabiralae2006@gmail.com>
2025-06-07 16:12:49 -0400

    Tutor+ commit

--- src/pages/ConfirmBooking.jsx
+++
@@ -1,8 +0,0 @@
-export default function ConfirmBooking() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h2>Confirmation</h2>
-      <p>Votre session est bien réservée avec John pour le jeudi 6 juillet à 18h. 🎉</p>
-    </div>
-  );
-}
